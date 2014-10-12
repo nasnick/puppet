064f6d7c30b375ad3fde05dab7beaaceebb451e8
root <root@localhost.localdomain>
2014-10-12 19:18:06 +1300

    added site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,21 @@
+import 'classes/*.pp'
+class toolbox {
+     file { '/usr/local/sbin/puppetsimple.sh':
+        owner => root, group => root, mode => 0755,
+        content => "#!/bin/sh\npuppet agent --onetime --no-daemonize --verbose $1",
+}
+}
+node 'webserver01-salt.us-west-2.compute.internal’{
+     include toolbox
+     include apache
+}
+
+
+
+ubuntu@puppetmaster-salt:~$ cat /etc/puppet/manifests/classes/apache.pp
+class apache {
+package { 'apache2-mpm-prefork':
+ensure => installed
+}
+
+}
